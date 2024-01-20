a38121e79b2e190a2b8888af5c26fb73eaab49e4
Ryan Tan <ryantan@rtyt16.local>
2024-01-20 20:04:33 +0800

    - Added dist folder output of application - Added basic text rank using SummaryBot

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// const SummaryBot = require('summarybot')
+import SummaryBot from 'summarybot';
+
 function App() {
   const [count, setCount] = useState(0)
+    const summarizer = new SummaryBot()
+
+    console.log(summarizer.run('bitch, sadsad.sadfmnkanfe.asdnkjnakdakjd.sdfnkajndf.askjndkjandknd.ejnrkwjnrejwn bfhbw4n.q mnr q3rnjnrajj cjkawe.', 5, false))
 
   return (
     <>
